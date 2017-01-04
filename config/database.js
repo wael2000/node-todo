e6d5bdd82006bae0c73e28a6c5178a2773dafbbb
wael2000 <waelibrahim2000@hotmail.com>
2017-01-04 13:00:56 +0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://admin:secret@172.30.163.197:27017/sampledb',
-    localUrl: 'mongodb://admin:secret@172.30.163.197:27017/sampledb'
+    remoteUrl : 'mongodb://admin:super-secret@172.30.163.197:27017/sampledb',
+    localUrl: 'mongodb://admin:super-secret@172.30.163.197:27017/sampledb'
 };
